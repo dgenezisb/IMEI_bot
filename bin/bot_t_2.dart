8b35ae68f9babbf890afd7829a4fdedcbf0ef193
Vov4ikINR <64402712+dgenezisb@users.noreply.github.com>
2022-09-30 21:20:28 +0300

    Add files via upload
    Добавил логи и еще одну проверку,какой э это пизедц....

--- bin/bot_t_2.dart
+++ bin/bot_t_2.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -7,14 +8,19 @@ import 'DataBase.dart';
 String upd = "";
 String updPr = "";
 var ifFirst = true;
+List updData = [];
+int lastUpdInt = 0;
+var ifOK = false;
 void main() async {
-  var DB = File('tac_database_June2022.csv');
-  var listOfDb = csvToList(DB);
+  //var DB = File('tac_database_June2022.csv');
+  //r listOfDb = csvToList(DB);
   var response;
   var insCount = 0;
   var imeis = IMEIS();
   var phones = PHONES();
-  var url = Uri.https('api.telegram.org', '/getUpdates');
+  List<String> log = [];
+  var url = Uri.https('api.telegram.org',
+      '/bot5729343686:AAGYrJnZ-jH6gWfMgui2ip88ejCjRZftKEA/getUpdates');
   int STOP = 1;
   int updCount = 0;
   //var ifFirst = true;
@@ -22,9 +28,15 @@ void main() async {
   try {
     while (STOP != 0) {
       loop:
+      if (log.length > 500) {
+        log.removeAt(0);
+      }
+
       response = await http.get(url);
       response = json.decode(response.body);
-      if (response != null) {
+      var NowTime = DateTime.now();
+      log.add("-->принял данные с апи $NowTime");
+      if ((response != null) && (response["result"] != null)) {
         Map result = <Map, String>{};
         if (ifFirst) {
           int i = 0;
@@ -34,6 +46,12 @@ void main() async {
           }
           updCount = i;
           updCountPrev = updCount - 1;
+          updData.add(response["result"][i]["update_id"].toString());
+          updData
+              .add(response["result"][i]["message"]["message_id"].toString());
+          updData.add(response["result"][i]["message"]["date"]);
+          lastUpdInt = i;
+          log.add("-->Заполнил первую дату апдейтов $NowTime");
         } else {
           int i = 0;
           while (response["result"][i].toString() !=
@@ -43,7 +61,33 @@ void main() async {
           if (i > updCount) {
             updCount++;
           }
+          lastUpdInt = updCount;
+          log.add("-->пока все ок...");
         }
+        //проверка по данным апдейта
+        if (updData.isNotEmpty) {
+          if ((updData[0] != response["result"].last["update_id"].toString()) &&
+              (updData[1] !=
+                  response["result"]
+                      .last["message"]["message_id"]
+                      .toString()) &&
+              updData[2] != response["result"].last["message"]["date"]) {
+            updData[0] = response["result"].last["update_id"].toString();
+            updData[1] =
+                response["result"].last["message"]["message_id"].toString();
+            updData[2] = response["result"].last["message"]["date"];
+            ifOK = true;
+            var id = response["result"].last["message_id"];
+            log.add(
+                "-->проверка по данным апдейта прошла успешно\n$NowTime\nupdate_id = $id");
+          } else {
+            ifOK = false;
+            if (ifFirst) {
+              ifOK = true;
+            }
+          }
+        }
+
         var lastUpd = response["result"][updCount]["message"];
         if (ifFirst) {
           upd = lastUpd['message_id'].toString();
@@ -52,8 +96,9 @@ void main() async {
           upd = lastUpd['message_id'].toString();
         }
 
-        if (upd != updPr) {
+        if ((upd != updPr) && (ifOK == true)) {
           if (updCount != updCountPrev) {
+            log.add("-->попал в цикл обрпботки данных $NowTime");
             var inp = lastUpd["text"];
             var altInp = inp;
             inp = Format(inp);
@@ -79,7 +124,9 @@ void main() async {
                     message.replaceAll(RegExp(" "), "");
               }
               var chatId = lastUpd["chat"]["id"];
-              var url = Uri.https('api.telegram.org', '//sendMessage',
+              var url = Uri.https(
+                  'api.telegram.org',
+                  '/bot5729343686:AAGYrJnZ-jH6gWfMgui2ip88ejCjRZftKEA/sendMessage',
                   {'chat_id': '$chatId', 'text': '$message\n\n$gLink'});
               http.post(url);
               updCountPrev++;
@@ -93,10 +140,13 @@ void main() async {
                 //gLink = spaceDel(message);
               }
               var chatId = lastUpd["chat"]["id"];
-              var url = Uri.https('api.telegram.org', '//sendMessage', {
-                'chat_id': '$chatId',
-                'text': 'Список найденых по запросу  IMEI\n\n$message'
-              });
+              var url = Uri.https(
+                  'api.telegram.org',
+                  '/bot5729343686:AAGYrJnZ-jH6gWfMgui2ip88ejCjRZftKEA/sendMessage',
+                  {
+                    'chat_id': '$chatId',
+                    'text': 'Список найденых по запросу  IMEI\n\n$message'
+                  });
               http.post(url);
               updCountPrev++;
             }
@@ -107,10 +157,14 @@ void main() async {
       }
     }
   } catch (e) {
-    var url = Uri.https('api.telegram.org', '//sendMessage',
-        {'chat_id': '444062880', 'text': 'ошибак $e'});
+    var url = Uri.https('api.telegram.org',
+        '/bot5729343686:AAGYrJnZ-jH6gWfMgui2ip88ejCjRZftKEA/sendMessage', {
+      'chat_id': '444062880',
+      'text': 'ошибак $e\n\nкаунтер апдейта $lastUpdInt\nлог всего говна\n$log'
+    });
     http.post(url);
+
     //updCount = updCount - 1;
-    main();
+    //main();
   }
 }
